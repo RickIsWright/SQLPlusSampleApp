a456f816068acc0da3fa0b0d7b031800a693720e
alan hyneman <alan.h.hyneman@gmail.com>
2020-01-14 02:12:32 -0500

    Updates to scripts and readme

--- SqlPlusDemo.Settings/ConnectionStrings.cs
+++ SqlPlusDemo.Settings/ConnectionStrings.cs
@@ -13,8 +13,7 @@ namespace SqlPlusDemo.Settings
             {
                 //Edit the server name and password
 
-                //return "Server = {server}; Initial Catalog = SqlPlusDemo; User ID = sa; Password = {password}";
-                return "Server = DESKTOP-E6COJ7P; Initial Catalog = SqlPlusDemo; User ID = sa; Password = 3apples";
+                return "Server = {server}; Initial Catalog = SqlPlusDemo; User ID = sa; Password = {password}";
             }
         }
     }
